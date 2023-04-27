99d2de51dbf180b9923d6da2ba2908ea08222e1f
Thomas Laffay <thom.laffay@gmail.com>
2023-04-27 17:28:20 +0200

    WIP Carroussel outils

---
+++ CVv2/circle.js
@@ -0,0 +1,19 @@
+const circleRadius = 100;
+const rotationSpeed = 0.005;
+const squares = document.querySelectorAll(".square");
+let angle = 0;
+
+function rotateSquares() {
+    for (let i = 0; i < squares.length; i++) {
+        let currentAngle = angle + (i * (2 * Math.PI / squares.length));
+        let x = circleRadius * Math.cos(currentAngle) - (squares[i].offsetWidth / 2);
+        let y = circleRadius * Math.sin(currentAngle) - (squares[i].offsetHeight / 2);
+
+        squares[i].style.transform = `translate(${x}px, ${y}px)`;
+    }
+    angle += rotationSpeed;
+
+    requestAnimationFrame(rotateSquares);
+}
+
+rotateSquares();
